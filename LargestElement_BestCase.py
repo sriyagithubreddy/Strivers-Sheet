0d8dd5eeaad70393eab824b4ffe232a317629a49
sriyagithubreddy <162871215+sriyagithubreddy@users.noreply.github.com>
2025-03-03 21:34:49 +0530

    Add files via upload

---
+++ LargestElement_BestCase.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def largest(self, arr: List[int]) -> int:
+        # The function finds the largest element in the input list 'arr'
+        
+        # Use Python's built-in max() function to return the largest element in the array
+        # The max() function automatically traverses the entire array and returns the largest value.
+        return max(arr)
+
